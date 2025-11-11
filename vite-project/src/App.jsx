4f7f23ff7d5a7f0747a719bcdafc0ba1ed0223b9
Webcloudxx <a.beshovishki@gmail.com>
2025-11-11 19:09:32 +0200

    did some testing

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import './App.css'
+
+function App() {
+    return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App
